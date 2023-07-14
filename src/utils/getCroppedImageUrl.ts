26426d5e4e31d98a92ff0ea07a4106ef0958391a
TheCharismaticEnigma <avishkar2002@mail.com>
2023-07-14 18:27:55 +0530

    Handle Games with no Image

--- src/utils/getCroppedImageUrl.ts
+++ src/utils/getCroppedImageUrl.ts
@@ -1,5 +1,7 @@
+import placholderImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url: string, width = '600', height = '400') => {
-  if (!url) return '';
+  if (!url) return placholderImage;
   const target = 'media/';
   const replacement = `${target}crop/${width}/${height}/`;
   return url.replace(target, replacement);
